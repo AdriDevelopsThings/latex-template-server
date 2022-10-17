f60906364faead0f001a076d948d43336483ecd4
AdriDevelopsThings <adrianbloober@gmail.com>
2022-10-17 20:28:51 +0200

    ups

--- pkg/template/builder.go
+++ pkg/template/builder.go
@@ -14,7 +14,7 @@ import (
 )
 
 func validateLatexValue(value string) string {
-	value = strings.ReplaceAll(value, "\\", "\textbackslash")
+	value = strings.ReplaceAll(value, "\\", "\\textbackslash")
 	return value
 }
 func BuildTemplate(name string, arguments map[string]string) (*files.FileInfos, error) {
